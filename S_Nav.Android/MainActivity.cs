f3334eda1fb74eec1554897982f6a9ca094d2b98
Jordan Way <jordanway3@hotmail.ca>
2020-12-06 09:24:27 -0500

    more minor changes

--- S_Nav.Android/MainActivity.cs
+++ S_Nav.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace S_Nav.Droid
 {
-    [Activity(Label = "S_Nav", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "S_Nav", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize, ScreenOrientation=ScreenOrientation.Portrait )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
